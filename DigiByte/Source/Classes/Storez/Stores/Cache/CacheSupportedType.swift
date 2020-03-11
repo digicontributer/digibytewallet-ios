a19848e5b5be0beab1e3870262bbdf58dc233f55
Yoshi <yoshi@jaeger.berlin>
2020-03-11 16:20:40 +0100

    Added Submodules, Connection icons and DigiAssets Address Importer

---
+++ DigiByte/Source/Classes/Storez/Stores/Cache/CacheSupportedType.swift
@@ -0,0 +1,60 @@
+//
+//  CacheSupportedType.swift
+//  Storez
+//
+//  Created by Mazyad Alabduljaleel on 12/8/15.
+//  Copyright © 2015 mazy. All rights reserved.
+//
+
+/** NSCache only requires objects to conform to AnyObject
+*/
+public typealias CacheSupportedType = AnyObject
+
+
+struct CacheSupportedBox <T: CacheSupportedType>: CacheTransaction {
+    
+    let value: T
+    
+    var supportedType: AnyObject? {
+        // FIXME: Compiler crash
+        let anyObject: AnyObject? = value
+        return anyObject
+    }
+    
+    init?(storedValue: AnyObject?) {
+        
+        guard let value = storedValue as? T else {
+            return nil
+        }
+        
+        self.value = value
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+}
+
+
+struct CacheNullableSupportedBox<T: Nullable>: CacheTransaction where T.UnderlyingType: CacheSupportedType {
+    
+    let value: T
+    
+    var supportedType: AnyObject? {
+        return value.wrappedValue
+    }
+    
+    init?(storedValue: AnyObject?) {
+        
+        guard let value = storedValue as? T.UnderlyingType else {
+                return nil
+        }
+        
+        self.value = T(value)
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+}
+
