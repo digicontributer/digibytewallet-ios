0d12070575cb3c19c06fa0a3a3100962dda86d76
Yoshi <yoshi@jaeger.berlin>
2020-05-14 17:53:38 +0200

    Added colours

--- DigiByte/Source/Extensions/UIColor+DAAdditions.swift
+++ DigiByte/Source/Extensions/UIColor+DAAdditions.swift
@@ -12,6 +12,7 @@ extension UIColor {
     /* digiassets */
     enum da {
         static let darkSkyBlue = UIColor(red: 38/255, green: 152/255, blue: 237/255, alpha: 1.0)
+        static let greenApple = UIColor(red: 76.0 / 255.0, green: 193.0 / 255.0, blue: 35.0 / 255.0, alpha: 1.0)
         static let secondaryGrey = UIColor(red: 47/255, green: 49/255, blue: 80/255, alpha: 1.0)
         static let assetBackground = UIColor(red: 35/255, green: 35/255, blue: 60/255, alpha: 1.0)
         static let inactiveColor = UIColor(red: 67/255, green: 68/255, blue: 90/255, alpha: 1.0)
@@ -19,5 +20,6 @@ extension UIColor {
         static let burnColor = UIColor(red: 220/255, green: 63/255, blue: 78/255, alpha: 1.0)
         
         static let backgroundColor = UIColor(red: 15/255, green: 15/255, blue: 43/255, alpha: 1.0)
+        static let headerBackgroundColor = UIColor(red: 35.0 / 255.0, green: 35.0 / 255.0, blue: 60.0 / 255.0, alpha: 1.0)
     }
 }
